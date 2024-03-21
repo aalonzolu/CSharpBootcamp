0869a917ba264739fca8656686893196b535ee6a
Andres Alonzo <andres.alonzo@bairesdev.com>
2024-03-21 15:03:10 -0600

    Level 2

---
+++ InventorySystem/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using InventorySystem.models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Book book = new Book("Avatar the Last Airbender", 100, "Michael Dante DiMartino");
+        Furniture furniture = new Furniture("Chair", 50, "Pine tree");
+        Electronics electronics = new Electronics("S22 Ultra", 700, "Samsung");
+        
+        Console.WriteLine("Item Information:");
+        book.DisplayInfo();
+        furniture.DisplayInfo();
+        electronics.DisplayInfo();
+    }
+}
---
+++ ShapesDrawing/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ShapesDrawing;
+
+using Shapes;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Circle circle = new Circle(5);
+        Rectangle rectangle = new Rectangle(8, 2);
+        
+        Console.WriteLine("Shapes Information:");
+        DrawAndCalculate(circle);
+        DrawAndCalculate(rectangle);
+    }
+
+    static void DrawAndCalculate(IShape shape)
+    {
+        shape.Draw();
+        Console.WriteLine($"Area: {shape.CalculateArea()}, Perimeter: {shape.CalculatePerimeter()}");
+    }
+}
